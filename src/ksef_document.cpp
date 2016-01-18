354b2eca2ef51ca01eecf95c30a6699d6f8b956f
Oleg Yurchenko <oleg.yurchenko@gmail.com>
2016-01-18 20:48:51 +0200

    Add ksef document

---
+++ src/ksef_document.cpp
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/**
+* @pkg ksef_document
+*/
+/**
+* Short description of ksef_document.
+*
+* Long description of ksef_document.<br>
+* (C) T&T, Kiev, Ukraine 2016.<br>
+* started 18.01.2016 15:43:30<br>
+* @pkgdoc ksef_document
+* @author oleg
+* @version 0.01 
+*/
+/*----------------------------------------------------------------------------*/
+#include "ksef_document.h"
+/*----------------------------------------------------------------------------*/
+
